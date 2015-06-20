ba0a61fc4094691acf743be234c99f3e4ba320af
刘红 <liuhong1.happy@163.com>
2015-06-20 20:37:52 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Dockerlover/docker-etcd.git
+cd docker-etcd
+curl -L  https://github.com/coreos/etcd/releases/download/v2.0.12/etcd-v2.0.12-linux-amd64.tar.gz -o etcd-v2.0.12-linux-amd64.tar.gz
+tar xzvf etcd-v2.0.12-linux-amd64.tar.gz etcd
+docker build -t docker-etcd .
